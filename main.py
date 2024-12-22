b27fc30e35daf1c86ca3fc0808263d79314f337b
gekas145 <56126336+gekas145@users.noreply.github.com>
2024-12-22 11:39:09 +0100

    add plot saving

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+import io
+import functools
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+
+# def animate(idx, ax, strokes):
+#     ax.plot(strokes[0, 0:idx], strokes[1, 0:idx])
+#     ax.set_xlim(np.min(strokes[0, :]), np.max(strokes[0, :]))
+#     ax.set_ylim(np.min(strokes[1, :]), np.max(strokes[1, :]))
 
 @st.cache_data
 def get_network_prediction(input_string):
@@ -22,18 +31,38 @@ def plot_network_prediction(network_prediction):
         current_input = st.session_state['last_valid_input']
 
     ax.set_title(current_input)
+
     return fig, current_input
+
+# def animate_network_prediction(network_prediction):
+#     fig, ax = plt.subplots()
+#     fig.set_size_inches(8, 4)
+
+#     user_text_input = st.session_state.get('text_input', None)
+
+#     current_input = None
+#     if user_text_input is None:
+#         current_input = DEFAULT_INPUT
+#     elif len(user_text_input) > 0:
+#         current_input = user_text_input
+#     else:
+#         current_input = st.session_state['last_valid_input']
+
+#     ax.set_title(current_input)
+
+#     anim = FuncAnimation(fig,
+#                          functools.partial(animate, ax=ax, strokes=network_prediction),
+#                          frames=10,
+#                          repeat=False)
+
+#     return anim, current_input
 
 DEFAULT_INPUT = 'hello there'
 
 with st.sidebar:
 
-    col1, col2 = st.columns(2, gap='medium')
-    with col1:
-        if st.button('Regenerate'):
-            get_network_prediction.clear()
-    with col2:
-        st.button('Replay')
+    if st.button('Regenerate'):
+        get_network_prediction.clear()
 
     st.markdown('#')
     
@@ -41,6 +70,8 @@ with st.sidebar:
                min_value=0.0,
                max_value=5.0,
                step=0.1)
+    
+    st.markdown('#')
 
 canvas = st.container()
 with canvas:
@@ -54,6 +85,18 @@ with canvas:
                   label_visibility='collapsed',
                   max_chars=32,
                   key='text_input')
+    
+png_plot = io.BytesIO()
+fig.savefig(png_plot, format='png')
 
+with st.sidebar:
+    col1, col2 = st.columns(2, gap='medium')
+    with col1:
+        st.download_button('Download PNG',
+                           data=png_plot,
+                           file_name=st.session_state['last_valid_input']+'.png',
+                           mime="image/png")
+    with col2:
+        st.button('Download GIF')
 
 
